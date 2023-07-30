5393f4b9ab85f09704b2301761e82a7e09165ee0
nek0d3r <darklink.perry@gmail.com>
2023-07-30 11:33:40 -0600

    day 12 completed extremely slowly

--- day12/solve.py
+++ day12/solve.py
@@ -12,11 +12,12 @@ class Node:
     prev: int = None
 
 content: str
-with open("input2.txt") as f:
+with open("input.txt") as f:
     content = f.read()
 
 rows = content.splitlines()
 start: int
+lowest = []
 end: int
 map = []
 for row in range(len(rows)):
@@ -33,47 +34,81 @@ for row in range(len(rows)):
             end = row * len(rows[row]) + col
         else:
             block = rows[row][col]
+        if block == 'a':
+            lowest.append(row * len(rows[row]) + col)
 
         north_block = '|' if row - 1 < 0 else rows[row - 1][col]
+        if north_block == 'S':
+            north_block = 'a'
+        if north_block == 'E':
+            north_block = 'z'
         south_block = '|' if row + 1 >= len(rows) else rows[row + 1][col]
+        if south_block == 'S':
+            south_block = 'a'
+        if south_block == 'E':
+            south_block = 'z'
         west_block = '|' if col - 1 < 0 else rows[row][col - 1]
+        if west_block == 'S':
+            west_block = 'a'
+        if west_block == 'E':
+            west_block = 'z'
         east_block = '|' if col + 1 >= len(rows[row]) else rows[row][col + 1]
+        if east_block == 'S':
+            east_block = 'a'
+        if east_block == 'E':
+            east_block = 'z'
 
-        if north_block in ['S', 'E'] or ord(north_block) <= ord(block) + 1:
+        if ord(north_block) <= ord(block) + 1:
             node.north = (row - 1) * len(rows[row]) + col
-        if south_block in ['S', 'E'] or ord(south_block) <= ord(block) + 1:
+        if ord(south_block) <= ord(block) + 1:
             node.south = (row + 1) * len(rows[row]) + col
-        if west_block in ['S', 'E'] or ord(west_block) <= ord(block) + 1:
+        if ord(west_block) <= ord(block) + 1:
             node.west = row * len(rows[row]) + col - 1
-        if east_block in ['S', 'E'] or ord(east_block) <= ord(block) + 1:
+        if ord(east_block) <= ord(block) + 1:
             node.east = row * len(rows[row]) + col + 1
         nodes.append(node)
     map.append(nodes)
 
 graph = list(itertools.chain(*map))
-print(graph)
 
-graph[start].dist = 0
-visited = []
-unvisited = [i for i in range(len(graph))]
-vertex = start
-while len(unvisited) > 0:
-    shortest = float("inf")
-    for node in unvisited:
-        if graph[node].dist < shortest:
-            vertex = node
-            shortest = graph[node].dist
-    
-    if graph[vertex].north in unvisited and graph[vertex].dist + 1 < graph[graph[vertex].north].dist:
-        graph[graph[vertex].north].dist = graph[vertex].dist + 1
-    if graph[vertex].east in unvisited and graph[vertex].dist + 1 < graph[graph[vertex].east].dist:
-        graph[graph[vertex].east].dist = graph[vertex].dist + 1
-    if graph[vertex].south in unvisited and graph[vertex].dist + 1 < graph[graph[vertex].south].dist:
-        graph[graph[vertex].south].dist = graph[vertex].dist + 1
-    if graph[vertex].west in unvisited and graph[vertex].dist + 1 < graph[graph[vertex].west].dist:
-        graph[graph[vertex].west].dist = graph[vertex].dist + 1
+paths = []
+for low in lowest:
+    for node in graph:
+        node.dist = float("inf")
+        node.prev = None
 
-    visited.append(vertex)
-    unvisited.remove(vertex)
+    graph[low].dist = 0
+    visited = []
+    unvisited = [i for i in range(len(graph))]
+    vertex = low
+    while len(unvisited) > 0:
+        shortest = float("inf")
+        vertex = unvisited[0]
+        for node in unvisited:
+            if graph[node].dist < shortest:
+                vertex = node
+                shortest = graph[node].dist
+        
+        if graph[vertex].north in unvisited and graph[vertex].dist + 1 < graph[graph[vertex].north].dist:
+            graph[graph[vertex].north].dist = graph[vertex].dist + 1
+            graph[graph[vertex].north].prev = vertex
+        if graph[vertex].east in unvisited and graph[vertex].dist + 1 < graph[graph[vertex].east].dist:
+            graph[graph[vertex].east].dist = graph[vertex].dist + 1
+            graph[graph[vertex].east].prev = vertex
+        if graph[vertex].south in unvisited and graph[vertex].dist + 1 < graph[graph[vertex].south].dist:
+            graph[graph[vertex].south].dist = graph[vertex].dist + 1
+            graph[graph[vertex].south].prev = vertex
+        if graph[vertex].west in unvisited and graph[vertex].dist + 1 < graph[graph[vertex].west].dist:
+            graph[graph[vertex].west].dist = graph[vertex].dist + 1
+            graph[graph[vertex].west].prev = vertex
 
-print(graph[end].dist)
+        visited.append(vertex)
+        # print(f"Removing {vertex} from {unvisited}")
+        unvisited.remove(vertex)
+
+    for node in graph:
+        # print(f"{graph.index(node)} ({node.value})\t{node.dist}\t{node.prev}")
+        if node.value == 'E':
+            paths.append(node.dist)
+
+print(f"Shortest steps to goal: {min(paths)}")
