a3fac519aff420219b82c437e90ba68d58d6d0a6
nek0d3r <darklink.perry@gmail.com>
2023-08-03 18:33:01 -0400

    day 13 example success but input problem

--- day13/solve.py
+++ day13/solve.py
@@ -1,19 +1,20 @@
-import ast
+import ast, os
 
-def compare(left, right):
+def compare(left, right, depth = 0):
     print(f"{left}<=>{right}")
     if type(left) is list and type(right) is int:
         print("Convert right to list")
-        compare(left, [right])
+        return compare(left, [right], depth + 1)
     elif type(left) is int and type(right) is list:
         print("Convert left to list")
-        compare([left], right)
+        return compare([left], right, depth + 1)
     elif type(left) is int and type(right) is int:
         if left < right:
             print("Inputs are in right order")
             return True
         elif left == right:
             print("Continue compare")
+            return None
         else:
             print("Inputs are in wrong order")
             return False
@@ -26,23 +27,40 @@ def compare(left, right):
     else:
         i = 0
         while i < len(left) and i < len(right):
-            compare(left[i], right[i])
+            result = compare(left[i], right[i], depth + 1)
+            if result != None:
+                return result
             i += 1
         if i == len(right) and i < len(left):
             print("Inputs are in wrong order")
             return False
+        elif depth > 0:
+            print("Continue compare")
+            return None
         else:
-            print("Inputs are in right order")
+            print("Ran out of items, inputs are in right order")
             return True
 
-with open("input2.txt") as f:
+with open("input.txt") as f:
+    index = 1
+    sum = 0
     while True:
-        left = ast.literal_eval(f.readline().strip())
-        right = ast.literal_eval(f.readline().strip())
+        left_packet = f.readline()
+        right_packet = f.readline()
         f.readline()
-        if not left or not right:
+        if not left_packet or not right_packet:
             break
+        left = ast.literal_eval(left_packet.strip())
+        right = ast.literal_eval(right_packet.strip())
         
         result = compare(left, right)
         print(result)
         print()
+        input()
+        os.system("cls")
+
+        if result:
+            sum += index
+        index += 1
+    
+    print(f"Pair indices sum: {sum}")
