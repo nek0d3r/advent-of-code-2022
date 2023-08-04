fca791188d45b27b23703002dd2114aa34e41d66
nek0d3r <darklink.perry@gmail.com>
2023-08-03 21:27:00 -0400

    day 13 complete

--- day13/solve.py
+++ day13/solve.py
@@ -1,4 +1,5 @@
 import ast
+from functools import cmp_to_key
 
 def compare(left, right, depth = 0):
     print(f"{left}<=>{right}")
@@ -11,53 +12,68 @@ def compare(left, right, depth = 0):
     elif type(left) is int and type(right) is int:
         if left < right:
             print("Inputs are in right order")
-            return True
+            return -1
         elif left == right:
             print("Continue compare")
-            return None
+            return 0
         else:
             print("Inputs are in wrong order")
-            return False
+            return 1
     else:
         i = 0
         while i < len(left) and i < len(right):
             result = compare(left[i], right[i], depth + 1)
-            if result != None:
+            if result != 0:
                 return result
             i += 1
         if i == len(right) and i < len(left):
             print("Inputs are in wrong order")
-            return False
+            return 1
         elif i == len(left) and i < len(right):
             print("Inputs are in right order")
-            return True
+            return -1
         elif depth > 0:
             print("Continue compare")
-            return None
+            return 0
         else:
             print("Ran out of items, inputs are in right order")
-            return True
+            return 0
 
-with open("C:\\Users\\darkl\\source\\repos\\advent-of-code-2022\\day13\\input.txt") as f:
-    index = 1
-    sum = 0
-    while True:
-        left_packet = f.readline()
-        right_packet = f.readline()
-        f.readline()
-        if not left_packet or not right_packet:
-            break
-        left = ast.literal_eval(left_packet.strip())
-        right = ast.literal_eval(right_packet.strip())
+def solve1(file):
+    with open(file) as f:
+        index = 1
+        sum = 0
+        while True:
+            left_packet = f.readline()
+            right_packet = f.readline()
+            f.readline()
+            if not left_packet or not right_packet:
+                break
+            left = ast.literal_eval(left_packet.strip())
+            right = ast.literal_eval(right_packet.strip())
+            
+            result = compare(left, right)
+            print(result)
+            print()
+
+            if result != 1:
+                sum += index
+            index += 1
         
-        result = compare(left, right)
-        print(result)
-        print()
-        if result == None:
-            input()
+        print(f"Pair indices sum: {sum}")
 
-        if result:
-            sum += index
-        index += 1
-    
-    print(f"Pair indices sum: {sum}")
+def solve2(file):
+    content: str
+    with open(file) as f:
+        content = f.read()
+    content = content.replace("\n\n", "\n")
+
+    packets = []
+    for line in content.splitlines():
+        packets.append(ast.literal_eval(line.strip()))
+    packets.append([[2]])
+    packets.append([[6]])
+    packets_ordered = sorted(packets, key=cmp_to_key(compare))
+    print(f"\nDecoder key: {(packets_ordered.index([[2]]) + 1) * (packets_ordered.index([[6]]) + 1)}")
+
+solve2("input.txt")
